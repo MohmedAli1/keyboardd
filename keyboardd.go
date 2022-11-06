4c7dd26f9693b07bceb250f9647eae301b1f2405
mohcs50 <88035831+mohcs50@users.noreply.github.com>
2022-11-06 23:03:10 +0200

    Add files via upload

---
+++ keyboardd.go
@@ -0,0 +1,23 @@
+package keyboardd
+
+import (
+	"bufio"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func GetFloat() (float64, error) {
+
+	m := bufio.NewReader(os.Stdin)
+	y, err := m.ReadString('\n')
+	if err != nil {
+		return 0, err
+	}
+	number, err := strconv.ParseFloat(strings.TrimSpace(y), 64)
+	if err != nil {
+		return 0, err
+	}
+	return number, nil
+
+}
